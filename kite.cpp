0529f1a9524657aeffbfb06a16ae579f5309e45e
Utkarsh Gupta <99074195+utkarsh1code@users.noreply.github.com>
2022-10-10 20:12:40 +0530

    Update kite.cpp
    made the changese now time complexity of code will be less. so which make it more efficient .

--- kite.cpp
+++ kite.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 int main()
 {
-    for(int i=0;i<5;i++)
+    for(int i=1;i<=5;i++)//i represent row number. 
     {
-    for(int j=0;j<5-i;j++)
+    for(int j=1;j<=5-i;j++)//j represent column number.
     {
         cout<<"* ";
     }
-    for (int j= 0; j<2*i; j++)
+    for (int j= 1; j<2*i; j++)
     {
         cout<<"  ";
     }
-    for(int j=0;j<5-i;j++)
+    for(int j=1;j<5-i;j++)
     {
         cout<<"* ";
     }
     cout<<endl;
     }
-     for(int i=0;i<5;i++)
+     for(int i=1;i<5;i++)
     {
-    for(int j=0;j<i+1;j++)
+    for(int j=1;j<i+1;j++)
     {
         cout<<"* ";
     }
-    for (int j= 0; j<8-2*i; j++)
+    for (int j= 1; j<8-2*i; j++)
     {
         cout<<"  ";
     }
-    for(int j=0;j<i+1;j++)
+    for(int j=1;j<i+1;j++)
     {
         cout<<"* ";
     }
